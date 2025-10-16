b8ce590c1495f5963a57cf70cddf4fe370871ff9
fedek2001 <fedek2001@gmail.com>
2025-10-15 21:39:12 -0300

    Update health_simple.py

--- api/health_simple.py
+++ api/health_simple.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.get("/")
 def root():
-    return {"ok": True, "who": "health_simple"}
+    return jsonify({"ok": True, "who": "health_simple"})
